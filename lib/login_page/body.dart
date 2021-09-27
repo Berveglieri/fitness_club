51dc19dbbc6d30112c442ae07e896934a20f9445
Felipe Costa <felipedimitri5@gmail.com>
2021-09-27 19:14:35 +0200

    added password icon and login button

--- lib/login_page/body.dart
+++ lib/login_page/body.dart
@@ -1,7 +1,14 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class Body extends StatelessWidget {
+class Body extends StatefulWidget {
+  @override
+  _BodyState createState() => _BodyState();
+}
+
+class _BodyState extends State<Body> {
+  bool _isHidden = true;
+
   @override
   Widget build(BuildContext context) {
     return Center(
@@ -12,8 +19,8 @@ class Body extends StatelessWidget {
         children: [
           Image.asset(
               'assets/images/logo/logo.png',
-            height: 350.0,
-            width: 350.0,
+            height: 300.0,
+            width: 300.0,
             alignment: Alignment.topCenter,
           ),
           const SizedBox(height: 9.0),
@@ -25,18 +32,44 @@ class Body extends StatelessWidget {
           ),
           const SizedBox(height: 14.0),
           TextFormField(
-            obscureText: true,
+            obscureText: _isHidden,
             enableSuggestions: false,
             autocorrect: false,
-            decoration: const InputDecoration(
-              icon: Icon(Icons.lock),
+            decoration: InputDecoration(
+              icon: const Icon(Icons.lock),
               hintText: 'password',
+              suffix: InkWell(
+                onTap: _togglePasswordView,
+                child: Icon(
+                    _isHidden ? Icons.visibility : Icons.visibility_off
+                ),
+              )
             ),
           ),
-          const SizedBox(height: 21.0),
+          const SizedBox(height: 51.0),
+         Container(
+           padding: const EdgeInsets.only(right: 120, left: 120, bottom: 15),
+           width: double.infinity,
+           child: ElevatedButton(
+             onPressed: () {},
+             style: ElevatedButton.styleFrom(
+               primary: Colors.black,
+               shape: RoundedRectangleBorder(
+                 borderRadius: BorderRadius.circular(30.0),
+               ),
+             ),
+             child: const Text('Login'),
+           ),
+         ),
         ],
       )
       ),
     );
+  }
+
+  void _togglePasswordView() {
+    setState(() {
+      _isHidden = !_isHidden;
+    });
   }
 }
